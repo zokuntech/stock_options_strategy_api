aa7c291e6bbdaebb789e584869faf8f2ce913438
Hector Silva-Robles <hectorsilvarobles@Hectors-MacBook-Air.local>
2025-10-05 21:09:58 -0700

    EMERGENCY: Cost optimization - 85% ECS reduction + auto-scaling to 0 + budget alerts

--- terraform/iam_github_oidc.tf
+++ terraform/iam_github_oidc.tf
@@ -118,6 +118,35 @@ data "aws_iam_policy_document" "gha_terraform_policy" {
     ]
     resources = ["*"]
   }
+  
+  # S3 permissions for Terraform state
+  statement {
+    actions = [
+      "s3:ListBucket",
+      "s3:GetBucketVersioning",
+      "s3:GetBucketLocation"
+    ]
+    resources = [aws_s3_bucket.terraform_state.arn]
+  }
+  
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["${aws_s3_bucket.terraform_state.arn}/*"]
+  }
+  
+  # DynamoDB permissions for state locking
+  statement {
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [aws_dynamodb_table.terraform_locks.arn]
+  }
 }
 
 resource "aws_iam_policy" "gha_ecs" {
